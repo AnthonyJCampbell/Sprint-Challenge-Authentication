a7efd798febc331e9e594648e3ec29e463e0f404
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-04-05 08:47:50 +0200

    Added register functionality

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
-
+const db = require('./../database/dbConfig')
 const { authenticate } = require('../auth/authenticate');
+const bcrypt = require('bcryptjs');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -9,7 +10,22 @@ module.exports = server => {
 };
 
 function register(req, res) {
-  
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10)
+  user.password = hash;
+  user.password = user.password.toLowerCase()
+  add(user)
+    .then(saved => {
+      res.status(201).json(saved)
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
+}
+
+async function add(user) {
+  const [id] = await db('users').insert(user)
+  return db('users').where({id}).first()
 }
 
 function login(req, res) {
